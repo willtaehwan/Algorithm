eac4c6fb08d0612fb02bba4e9fad402c0889ff90
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2023-10-27 17:21:03 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int A, B;
+
+
+int main() {
+
+	cin >> A >> B;
+
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B << endl;
+	
+	return 0;
+}
