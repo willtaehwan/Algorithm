5abc2831eaf5baa77adf548d2449f689ce33c38f
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2023-12-20 20:45:57 +0900

    [Bronze I] Title: 단어 공부, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int DAT[26] = { 0, };
+
+int main() {
+
+	string row;
+	cin >> row;
+	
+	for (int i = 0; i < row.size(); i++) {
+		int ans = 0;
+		if (row[i] < 91) ans = int(row[i] - 'A');
+		else ans = int(row[i] - 'a');
+		DAT[ans]++;
+	}
+	int maxSum = 0;
+	int maxChar = 0;
+	int flag = 0;
+	for (int i = 0; i < 26; i++) {
+		if (maxSum < DAT[i]) {
+			maxSum = DAT[i];
+			maxChar = i;
+			flag = 1;
+			continue;
+		}
+		
+		if (maxSum == DAT[i] && maxSum != 0) {
+			flag = 2;
+		}
+	}
+
+	if (flag == 1) cout << char('A' + maxChar);
+	else cout << "?";
+
+	return 0;
+}
