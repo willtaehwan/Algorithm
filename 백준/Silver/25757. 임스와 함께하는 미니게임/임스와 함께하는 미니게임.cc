daed3cb6c1c91a12db861d1b1e87ae1c8629d7c5
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-06-17 17:03:30 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 56 ms, Memory: 8716 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<string> name;
+
+int N;
+char g;
+
+int game(char g) {
+	int num = 0;
+	if (g == 'Y') num = 1;
+	if (g == 'F') num = 2;
+	if (g == 'O') num = 3;
+
+	return num;
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> g;
+	for (int i = 0; i < N; i++) {
+		string now;
+		cin >> now;
+		name.push_back(now);
+	}
+
+	sort(name.begin(), name.end());
+	name.erase(unique(name.begin(), name.end()), name.end());
+
+	cout << name.size() / game(g);
+
+	return 0;
+}
