b2ecc9f7df4e123c39b80e43989fe8022bfe63e7
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-06-18 10:38:38 +0900

    [Silver IV] Title: 등수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.cc
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.cc
@@ -4,19 +4,19 @@ using namespace std;
 
 int main() {
 
-	int N, score, P;
-	cin >> N >> score >> P;
+	int N, s, P;
+	cin >> N >> s >> P;
 
-	int rank = 1, p = 0;
+	int r = 1, p = 0;
 	int n;
 
 	for (int i = 0; i < N; i++){
 		cin >> n;
-		if (n > score) rank++;
+		if (n > s) r++;
 	}
 
-	if (N == P && n >= score) rank = -1;
-	cout << rank;
+	if (N == P && n >= s) r = -1;
+	cout << r;
 	
 	return 0;
 }
