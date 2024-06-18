1ef1d9402abf90e4c96c81f148c86ebd74cba91f
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-06-18 10:50:54 +0900

    [Silver IV] Title: 등수 구하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.cc
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.cc
@@ -1,11 +1,2 @@
 #include <stdio.h>
-int main(){
-	int N,s,P,p,n,r=1;
-	scanf("%d %d %d",&N,&s,&P);
-	for(int i=0;i<N;i++){
-		scanf("%d",&n);
-		if(n>s)r++;
-	}
-	if(N==P&&n>=s)r=-1;
-	printf("%d",r);
-}
+int main(){int N,s,P,p,n,i,r=1;scanf("%d %d %d",&N,&s,&P);for(i=0;i<N;i++){scanf("%d",&n);if(n>s)r++;}if(N==P&&n>=s)r=-1;printf("%d",r);}
