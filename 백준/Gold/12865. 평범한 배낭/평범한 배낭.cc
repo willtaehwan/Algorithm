4e5a893f4db44233a5655c1170aa561e44c59c5a
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-02 01:15:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 28 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -2,20 +2,8 @@
 #include <algorithm>
 using namespace std;
 
-/*
-row = 물건의 index
-col = 무게
-
-*/
-
-struct item {
-	int w;
-	int v;
-};
-
 int N, K;
-
-item arr[101];
+int w, v;
 
 int DP[101][100010] = { 0, };
 
@@ -23,14 +11,11 @@ int main() {
 
 	cin >> N >> K;
 
-
-	for (int i = 1; i <= N; i++) cin >> arr[i].w >> arr[i].v;
-
 	for (int i = 1; i <= N; i++) {
+		cin >> w >> v;
 		for (int j = 1; j <= K; j++) {
-			if (j - arr[i].w >= 0) DP[i][j] = max(DP[i - 1][j - arr[i].w] + arr[i].v, DP[i-1][j]);
+			if (j - w >= 0) DP[i][j] = max(DP[i - 1][j - w] + v, DP[i-1][j]);
 			else DP[i][j] = DP[i - 1][j];
-			
 		}
 	}
 
