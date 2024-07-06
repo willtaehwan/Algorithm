38b840bf0373f4a1f2edf2585cf2169d713e4210
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-06 14:36:12 +0900

    [Gold I] Title: 이항 계수 3, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
@@ -1,25 +1,47 @@
 #include <iostream>
 #define MOD 1000000007
+#define ll unsigned long long
+
 using namespace std;
 
 int N, K;
 
-long long temp;
-
-long long expr(long long a, long long m) {
+ll expr(ll a, ll m) {
 	
-	if (m == 0) return 1;
+	a %= MOD;
+	ll temp = 1;
 
-	temp = expr(a, m / 2) % MOD;
-	if (m % 2 == 0) return temp * temp % MOD;
-	else return temp * temp % MOD * a % MOD ;
+	while (m > 0) {
+		if (m % 2 == 1) {
+			temp *= a;
+			temp %= MOD;
+		}
+
+		a *= a;
+		a %= MOD;
+		m /= 2;
+	}
+
+	return temp;
 }
 
 int main() {
 
 	cin >> N >> K;
 
-	long long A = 1, B = 1, result;
+	if (K == 0 || K == N) {
+		cout << 1;
+		return 0;
+	}
+
+	if (K == 1 || N == 1) {
+		cout << N;
+		return 0;
+	}
+
+	if (K > N / 2) K = N - K;
+
+	ll A = 1, B = 1, result;
 
 	for (int i = N; i >= N - K + 1; i--) A = (A * i) % MOD;
 
