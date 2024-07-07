128a87d6f63100ea12cb0487eabe80504e96352b
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-07 19:48:44 +0900

    [Platinum IV] Title: 이항 계수 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/11402. 이항 계수 4/이항 계수 4.cc
+++ 백준/Platinum/11402. 이항 계수 4/이항 계수 4.cc
@@ -2,21 +2,12 @@
 #define ull unsigned long long
 using namespace std;
 
-/*
-뤼카의 정리
-1. N과 K를 M진수로 변환
-2. 변환되어 생성된 N과 K의 계수 배열은 같은 index끼리 이항 계수를 구함 0<= 계수 < M 
-3. 이 이항 계수는 M 이하의 자연수이고, M은 소수이기 때문에 두 수는 서로소임이 보장됨
-4. 따라서, 페르마 소정리로 풀면 됨
-
-*/
-
 ull N, K;
 int M;
 int cnt = 0;
-int arr_N[201];
-int arr_K[201];
-int MOD[201];
+int arr_N[40];
+int arr_K[40];
+int MOD[40];
 
 void baseM() {
 
@@ -28,7 +19,6 @@ void baseM() {
 		arr_K[cnt] = temp_K % M;
 		temp_N /= M;
 		temp_K /= M;
-		// cout << arr_N[cnt] << " " << arr_K[cnt] << '\n';
 		cnt++;
 	}
 }
@@ -61,9 +51,7 @@ void binom(int n, int k, int idx) {
 
 	if (n - k < k) k = n - k;
 
-	int A = 1;
-	int B = 1;
-	
+	int A = 1, B = 1;
 	
 	for (int i = n; i >= n - k + 1; i--) A = (A * i) % M;
 	for (int i = 1; i <= k; i++) B = (B * i) % M;
@@ -84,6 +72,6 @@ int main() {
 	for (int i = 0; i < cnt; i++) result = (result * MOD[i]) % M;
 
 	cout << result;
-
+	
 	return 0;
 }
