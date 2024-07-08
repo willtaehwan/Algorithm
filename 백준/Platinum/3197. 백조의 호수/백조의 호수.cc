e24bde6fef0da35efe08e50f4c30cf90f06712ec
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-08 13:40:46 +0900

    [Platinum V] Title: 백조의 호수, Time: 152 ms, Memory: 25308 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
@@ -8,7 +8,6 @@ struct Node {
 };
 
 int R, C;
-int map[1510][1510] = { 0, };
 int visited[1510][1510] = { 0, };
 int d_visited[1510][1510] = { 0, };
 
@@ -17,46 +16,11 @@ int dc[] = { 0,0,-1,1 };
 
 Node swan[2];
 
-int min_dist = 100001;
 bool meet_flag = false;
 
 queue<Node> q;
+queue<Node> qq;
 queue<Node> nextq;
-
-void melting_bfs() {
-
-	queue<Node> qq;
-
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
-			if (map[i][j] == 0) {
-				qq.push({ i,j });
-				visited[i][j] = 1;
-			}
-		}
-	}
-
-	while (!qq.empty()) {
-		Node now = qq.front(); qq.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int ner = now.row + dr[i];
-			int nec = now.col + dc[i];
-
-			if (ner < 0 || nec < 0 || ner >= R || nec >= C) continue;
-			
-			if (visited[ner][nec] != 0) continue;
-
-			visited[ner][nec] = visited[now.row][now.col] + 1;
-
-			qq.push({ ner,nec });
-
-		}
-	}
-}
-
-
-
 
 void bfs_ch(int cnt) {
 	
@@ -64,7 +28,6 @@ void bfs_ch(int cnt) {
 		Node el = nextq.front(); nextq.pop();
 		q.push(el);
 	}
-
 
 	while (!q.empty()) {
 		Node now = q.front(); q.pop();
@@ -74,11 +37,10 @@ void bfs_ch(int cnt) {
 		for (int i = 0; i < 4; i++) {
 			int ner = now.row + dr[i];
 			int nec = now.col + dc[i];
-			//cout << ner << " , " << nec << '\n';
+
 			if (ner < 0 || nec < 0 || ner >= R || nec >= C) continue;
 			if (d_visited[ner][nec] != 0) continue;
 			if (visited[ner][nec] == cnt + 1) {
-				//cout << "hi\n";
 				visited[ner][nec] = -1;
 				nextq.push({ ner,nec });
 				continue;
@@ -90,9 +52,7 @@ void bfs_ch(int cnt) {
 			q.push({ ner,nec });
 
 		}
-
 	}
-
 }
 
 
@@ -107,36 +67,38 @@ int main() {
 			char now;
 			cin >> now;
 			if (now == 'L') swan[cnt++] = { i,j };
-			if (now == 'X') map[i][j] = 1;
+			if (now == 'X') continue;
+			qq.push({ i,j });
+			visited[i][j] = 1;
 		}
 	}
+	
+	while (!qq.empty()) {
+		Node now = qq.front(); qq.pop();
 
+		for (int i = 0; i < 4; i++) {
+			int ner = now.row + dr[i];
+			int nec = now.col + dc[i];
+
+			if (ner < 0 || nec < 0 || ner >= R || nec >= C) continue;
+
+			if (visited[ner][nec] != 0) continue;
+
+			visited[ner][nec] = visited[now.row][now.col] + 1;
+
+			qq.push({ ner,nec });
+
+		}
+	}
+	
 	nextq.push(swan[0]);
 	d_visited[swan[0].row][swan[0].col] = 1;
 	int mel_cnt = 0;
-	
-	melting_bfs();
-	/*
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
-			cout << visited[i][j] - 1 << " ";
-		}
-		cout << '\n';
-	}
-	cout << "=================================\n";
-	*/
+
 	while (!meet_flag) {
 		
 		bfs_ch(++mel_cnt);
-		/*
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				cout << d_visited[i][j] << " ";
-			}
-			cout << '\n';
-		}
-		cout << '\n';
-		*/
+		
 		if (meet_flag) cout<< mel_cnt -1 ;
 	}
 	
