3c1fb0bca702fd65c1baa40758c6149ce6449796
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-16 13:59:20 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 32 ms, Memory: 3052 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -8,33 +8,31 @@ int N;
 
 int main() {
 
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
 	while (1) {
 
 		cin >> N;
+
 		if (N == 0) break;
+
 		vector<ll> v(N + 2);
-		ll result = 0;
-		v[0] = 0;
-		v[N+1] = 0;
+		v[0] = v[N + 1] = 0;
+
 		for (int i = 1; i <= N; i++) cin >> v[i];
 
 		stack<ll> s;
+		ll result = 0;
 
-		for (int i = 1; i <= N+1; i++) {
-			
+		for (int i = 1; i <= N + 1; i++) {
 			while (!s.empty() && v[s.top()] >= v[i]) {
 				int temp = s.top(); s.pop();
 				int width = i - ((s.empty()) ? 0 : s.top()) - 1;
 				if (result < v[temp] * width) result = v[temp] * width;
-				//cout << i << " " << v[temp] << " " << width<<'\n';
 			}
 			s.push(i);
 		}
-
 		cout << result << '\n';
-
 	}
-
-
 	return 0;
 }
