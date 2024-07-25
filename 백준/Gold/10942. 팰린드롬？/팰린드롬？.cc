db6883c608b890551d32ca512f91879d3f8b124f
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-25 19:10:55 +0900

    [Gold IV] Title: 팰린드롬?, Time: 236 ms, Memory: 5972 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -4,9 +4,9 @@ using namespace std;
 
 int N, M;
 
-int arr[2001];
+int arr[2010];
 
-bool dp[2001][2][1010] = {0,};
+bool dp[2010][2010] = {0,};
 
 int main() {
 
@@ -14,46 +14,24 @@ int main() {
 
 	cin >> N;
 
-	for (int i = 1; i <= N; i++) cin >> arr[i];
-
 	for (int i = 1; i <= N; i++) {
-		
-		for (int k = 0; k < 2; k++) {
-			bool flag = true;
-			
-			for (int j = 0; j <= N / 2; j++) {
-				int left = i - j;
-				int right = i + j;
-				if (k == 1) right = i+j+1;
-				if (left<0 || right>N) break;
-				if (arr[left] != arr[right]) flag = false;
-				if (!flag) break;
-				dp[i][k][j] = flag;
-			}
+		cin >> arr[i];
+		dp[i][i] = 1;
+		if (arr[i - 1] == arr[i]) dp[i - 1][i] = 1;
+	}
+
+	for (int i = 2; i <= N - 1; i++) {
+		for (int j = 1; i + j <= N; j++) {
+			if (arr[j] == arr[i + j] && dp[j + 1][i + j - 1] == 1) dp[j][i + j] = 1;
 		}
 	}
-	/*
-	cout << '\n';
-	for (int i = 0; i <= N; i++) {
-		for (int k = 0;k < 2; k++) {
-			cout << "   ";
-			for (int j = 0; j <= N / 2; j++) {
-				cout << dp[i][k][j] << " ";
-			}
-		}
-		cout << '\n';
-	}
-	*/
+	
 	cin >> M;
 
 	for (int i = 0; i < M; i++) {
 		int S, E;
 		cin >> S >> E;
-		int m = (E + S) / 2;
-		int l = m - S;
-		int k = 0;
-		if ((E + S) % 2 == 1) k = 1;
-		cout << dp[m][k][l]<<'\n';
+		cout << dp[S][E]<<'\n';
 	}
 
 
