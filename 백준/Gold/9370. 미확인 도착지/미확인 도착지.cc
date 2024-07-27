6afc0d314ee1e503977d324713323bf83a62ec35
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-27 12:33:59 +0900

    [Gold II] Title: 미확인 도착지, Time: 140 ms, Memory: 3400 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
@@ -50,7 +50,7 @@ void bfs() {
 }
 
 int main() {
-	//ios_base::sync_with_stdio(false); cin.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> T;
 
 	while (T--) {
