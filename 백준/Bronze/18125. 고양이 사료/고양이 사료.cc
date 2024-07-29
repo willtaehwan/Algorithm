39c0968a5d3c9e175159593384936ea2581713c8
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-29 13:19:27 +0900

    [Bronze I] Title: 고양이 사료, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Bronze/18125. 고양이 사료/고양이 사료.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int R, C;
+
+int map[101][101];
+int tmap[101][101];
+
+int main() {
+
+	cin >> C >> R;
+
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	for (int i = 0; i < C; i++) {
+		for (int j = 0; j < R; j++) {
+			cin >> tmap[i][j];
+		}
+	}
+	bool flag = true;
+	for (int i = 0; i < R; i++) {
+		if (!flag) break;
+		for (int j = 0; j < C; j++) {
+			if (map[i][j] != tmap[j][R-i-1]) {
+				flag = false;
+				break;
+			}
+		}
+	}
+
+	if (flag) {
+		cout << "|>___/|        /}\n";
+		cout << "| O < |       / }\n";
+		cout << "(==0==)------/ }\n";
+		cout << "| ^  _____    |\n";
+		cout << "|_|_/     ||__|\n";
+	}
+	else {
+		cout << "|>___/|     /}\n";
+		cout << "| O O |    / }\n";
+		cout << "( =0= )\"\"\"\"  \\\n";
+		cout << "| ^  ____    |\n";
+		cout << "|_|_/    ||__|\n";
+	}
+
+	return 0;
+}
