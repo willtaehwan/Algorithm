574375dffe4c6fe26703ec0a61bd693dce5a4ae8
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-07-31 22:59:16 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 12 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.cc
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.cc
@@ -1,16 +1,16 @@
 #include <iostream>
-#define MOD 20000303
+
 using namespace std;
 
 string str;
 
 int main() {
-	
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> str;
 
 	int rem = 0;
 	for (int i = 0; i < str.size(); i++) {
-		rem = (rem * 10 + (str[i] - '0')) % MOD;
+		rem = (rem * 10 + (str[i] - '0')) % 20000303;
 	}
 	cout << rem;
 
