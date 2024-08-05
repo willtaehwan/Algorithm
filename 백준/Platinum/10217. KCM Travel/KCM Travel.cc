737ab5c574693f0173fc64962a45c881b696e632
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-08-06 07:35:17 +0900

    [Platinum IV] Title: KCM Travel, Time: 464 ms, Memory: 7044 KB -BaekjoonHub

--- 백준/Platinum/10217. KCM Travel/KCM Travel.cc
+++ 백준/Platinum/10217. KCM Travel/KCM Travel.cc
@@ -1,78 +1,60 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include<algorithm>
+#define INF 1e9
+#define MAX 10001
 using namespace std;
-#define INF 1000000000
 
-int N,M,K;
-vector<pair<int,pair<int,int>>>v[101];
-int dist[101][10001];
+struct Node {
+	int end;
+	int cost;
+	int time;
+};
 
-void dijkstra()
-{
-    queue<pair<int,pair<int,int>>>q;
-    q.push({0,{1,0}});
-    dist[1][0] = 0;
-    
-    while(!q.empty())
-    {
-        int curtime = q.front().first;
-        int cur = q.front().second.first;
-        int curcost = q.front().second.second;
-        q.pop();
-        if(dist[cur][curcost] < curtime)    continue;
-        for(int i=0; i<v[cur].size(); i++)
-        {
-            int next = v[cur][i].first;
-            int nextcost = v[cur][i].second.first + curcost;
-            int nexttime = v[cur][i].second.second + curtime;
-            if(nextcost <= M && dist[next][nextcost] > nexttime)
-            {
-                for(int j=nextcost+1; j<=M; j++)
-                {
-                    if(dist[next][j] <= nexttime)    break;
-                    dist[next][j] = nexttime;
-                }
-                dist[next][nextcost] = nexttime;
-                q.push({nexttime,{next,nextcost}});
-            }
-        }
-    }
+int T, N, M, K;
+
+int dist[MAX][105] = { 0, };
+
+vector<Node> v[105];
+
+int dp(int nowCost, int nowNode) {
+	int& ret = dist[nowCost][nowNode];
+	if (ret != -1) return ret;
+	if (nowNode == N) return ret = 0;
+
+	ret = INF;
+	for (int i = 0; i < v[nowNode].size(); i++) {
+		Node next = v[nowNode][i];
+		if (nowCost >= next.cost) ret = min(ret, dp(nowCost - next.cost, next.end) + next.time);
+	}
+	return ret;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int T;
-    cin >> T;
-    while(T--)
-    {
-        cin >> N >> M >> K;
-        for(int i=0; i<K; i++)
-        {
-            int from,to,cost,t;
-            cin >> from >> to >> cost >> t;
-            v[from].push_back({to,{cost,t}});
-        }
-        
-        for(int i=1; i<=N; i++)
-        {
-            for(int j=0; j<=M; j++) dist[i][j] = INF;
-        }
-        
-        dijkstra();
-        
-        int ans = INF;
-        for(int i=0; i<=M; i++)
-        {
-            if(dist[N][i] < ans)
-            {
-                ans = dist[N][i];
-            }
-        }
-        if(ans == INF)  cout<<"Poor KCM";
-        else    cout<<ans;
-    }
+int main() {
+
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	cin >> T;
+
+	cin >> N >> M >> K;
+
+	for (int i = 0; i < K; i++) {
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		v[a].push_back({ b,c,d });
+	}
+
+	for (int i = 0; i <= M; i++) {
+		for (int j = 0; j <= N; j++) {
+			dist[i][j] = -1;
+		}
+	}
+
+	int result = dp(M, 1);
+
+	if (result == INF) cout << "Poor KCM";
+	else cout << result;
+
+	return 0;
 }
