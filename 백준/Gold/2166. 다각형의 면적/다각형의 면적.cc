0d139a8f42fa0d957a43e5b3e77d1353d0a8b3e8
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-08-09 12:10:24 +0900

    [Gold V] Title: 다각형의 면적, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+
+	cin >> N;
+
+	long double result = 0.0;
+	
+	long long o_x, o_y;
+	cin >> o_x >> o_y;
+	
+	long long t_x, t_y;
+	cin >> t_x >> t_y;
+
+	for (int i = 0; i < N-2; i++) {
+		long long x, y;
+		cin >> x >> y;
+		result += (o_x * t_y + t_x * y + x * o_y - t_x * o_y - x * t_y - o_x * y) * 0.5;
+		t_x = x;
+		t_y = y;
+	}
+	cout << fixed;
+	cout.precision(1);
+	cout << abs(result);
+
+	return 0;
+}
