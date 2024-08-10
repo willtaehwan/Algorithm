0eb721d948c2d14d785b603c5e97717fa71179c3
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-08-10 14:22:13 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -1,67 +1,35 @@
 #include <iostream>
-#include <algorithm>
-using namespace std;
 
-struct Node {
-	int sum;
-	int re;
-};
+using namespace std;
 
 int N;
 
-long long arr[100010];
+int arr[100010];
 
 int min_result = 2e9 + 1;
-
-Node searchN(int n) {
-
-	int left = n + 1;
-	int right = N - 1;
-	int mid = 0;
-	int res = 0;
-
-	int min_r = 2e9 + 1;
-	int min_m = 0;
-
-	while (left <= right) {
-		
-		mid = (left + right) / 2;
-		res = arr[mid] + arr[n];
-		//cout << n << "  / " << left << ", " << right <<"  mid = " << mid << " result : " << res << '\n';
-		if (min_r > abs(res)) {
-			min_r = abs(res);
-			min_m = mid;
-		}
-		if (left == right) break;
-		if (res > 0) right = mid;
-		if (res < 0) left = mid+1;
-		if (res == 0) break;
-
-	}
-
-
-	return { min_r, min_m };
-}
-
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
 
 	for (int i = 0; i < N; i++) cin >> arr[i];
+	
+	int left = 0;
+	int right = N - 1;
 
-	int ans1 = -1;
-	int ans2 = -1;
+	int ans1, ans2;
 
-	for (int i = 0; i < N - 1; i++) {
-		Node result = searchN(i);
-		if (min_result > result.sum) {
-			//cout <<"          " << i << " , " << result.re<<"  res = "<<result.sum << '\n';
-			min_result = result.sum;
-			ans1 = arr[i];
-			ans2 = arr[result.re];
+	while (right != left) {
+		int res = arr[left] + arr[right];
+		if (min_result > abs(res)) {
+			min_result = abs(res);
+			ans1 = arr[left];
+			ans2 = arr[right];
 		}
+
 		if (min_result == 0) break;
+		if (res > 0) right--;
+		if (res < 0) left++;
 	}
 
 	cout << ans1 << " " << ans2;
