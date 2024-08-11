4ad9805004af7cc0d95c98c2db962bd6d7f6594a
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-08-12 00:12:40 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 104 ms, Memory: 3236 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Edge{
+	int a;
+	int b;
+	int cost;
+
+	bool operator<(Edge left) const {
+		if (cost < left.cost) return true;
+		else return false;
+	}
+
+};
+
+int V, E;
+
+Edge ed[100001];
+int parent[10010];
+
+int Find(int A) {
+	if (parent[A] == A) return A;
+	return parent[A] = Find(parent[A]);
+}
+
+void Union(int A, int B) {
+
+	int rootA = Find(A);
+	int rootB = Find(B);
+
+	if (rootA == rootB) return;
+
+	parent[rootA] = rootB;
+}
+
+int main(){
+
+	cin >> V >> E;
+
+	for (int i = 0; i < E; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		ed[i] = { a,b,c };
+	}
+
+	sort(ed, ed + E);
+
+	//for (int i = 0; i < E; i++) cout << ed[i].cost << " ";
+	//cout << '\n';
+
+	for (int i = 0; i <= V; i++) parent[i] = i;
+
+	int sum = 0;
+	for (int i = 0; i < E; i++) {
+		Edge now = ed[i];
+
+		if (Find(now.a) == Find(now.b)) continue;
+
+		Union(now.a, now.b);
+		sum += now.cost;
+	}
+
+	cout << sum;
+
+	return 0;
+}
