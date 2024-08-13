274d4da4b671c5e0c3616516e1c7fbd2f6b9eabb
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-08-13 15:18:55 +0900

    [Gold III] Title: 줄 세우기, Time: 84 ms, Memory: 3956 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, M;
+
+int degree[32010] = { 0, };
+
+vector<int> v[32010];
+
+int main() {
+
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		degree[b]++;
+	}
+
+	queue<int> q;
+	queue<int> result;
+	for (int i = 1; i <= N; i++) {
+		if (degree[i] == 0) q.push(i);
+	}
+
+	while (!q.empty()) {
+		int now = q.front(); q.pop();
+	
+		result.push(now);
+			
+		for (int i = 0; i < v[now].size(); i++) {
+			int next = v[now][i];
+			if (degree[next] > 1) degree[next]--;
+			else q.push(next);
+		}
+	}
+
+	while (!result.empty()) {
+		int now = result.front(); result.pop();
+		cout << now <<" ";
+	}
+
+
+	return 0;
+}
