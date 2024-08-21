f6ef5494a87eedf29d374c70d6af611d529d1eef
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-08-21 20:33:10 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 92 ms, Memory: 6456 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.cc
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.cc
@@ -12,7 +12,6 @@ struct Node {
 int N, M;
 
 int map[1001][1001] = { 0, };
-int visited[1001][1001] = { 0, };
 
 vector<int> v;
 
@@ -24,7 +23,7 @@ void bfs(int row, int col, int num) {
 	int cnt = 1;
 	queue<Node> q;
 	q.push({ row, col });
-	visited[row][col] = num;
+	map[row][col] = -num;
 
 	while (!q.empty()) {
 		Node now = q.front(); q.pop();
@@ -32,38 +31,29 @@ void bfs(int row, int col, int num) {
 		for (int i = 0; i < 4; i++) {
 			int ner = now.row + dr[i];
 			int nec = now.col + dc[i];
-
 			if (ner < 0 || nec < 0 || ner >= N || nec >= M) continue;
-			if (visited[ner][nec] != 0) continue;
-			if (map[ner][nec] == 1) continue;
-			visited[ner][nec] = num;
+			if (map[ner][nec] != 0) continue;
+			map[ner][nec] = -num;
 			q.push({ ner,nec });
 			cnt++;
 		}
 	}
-
 	v.push_back(cnt);
-
 }
 
 int check(int row, int col) {
-	//cout << "==========================================\n";
-	//cout << row << " " << col << "  :  ";
+
 	int result = 1;
 	set<int> s;
 	for (int i = 0; i < 4; i++) {
 		int ner = row + dr[i];
 		int nec = col + dc[i];
 		if (ner < 0 || nec < 0 || ner >= N || nec >= M) continue;
-		if (map[ner][nec]) continue;
-		s.insert(visited[ner][nec]);
+		if (map[ner][nec] == 1) continue;
+		s.insert(-map[ner][nec]);
 	}
 
-	for (auto it = s.begin();it != s.end();it++) {
-		result += v[*it - 1];
-		//cout << *it << " ";
-	}
-	//cout << "============================\n";
+	for (auto it : s) result += v[it - 1];
 	return result % 10;
 }
 
@@ -83,21 +73,9 @@ int main() {
 	int area_idx = 1;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (map[i][j] == 0 && visited[i][j] == 0) bfs(i, j, area_idx++);
+			if (map[i][j] == 0) bfs(i, j, area_idx++);
 		}
 	}
-
-	/*for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cout << visited[i][j] << " ";
-		}
-		cout << '\n';
-	}
-
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << " ";
-	}
-	cout << '\n';*/
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
@@ -106,8 +84,6 @@ int main() {
 		}
 		cout << '\n';
 	}
-
-	
 
 	return 0;
 }
