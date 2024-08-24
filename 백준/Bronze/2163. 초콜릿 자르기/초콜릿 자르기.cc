2273a7736e1bb50757a5b5e201421c222b1c343f
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-08-24 19:40:04 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -1,30 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
-int arr[90001];
-
-int ans(int now) {
-
-	if (arr[now] != -1) return arr[now];
-
-	int result = 0;
-	if (now % 2 == 1) result = ans(now / 2) + ans(now / 2 + 1) + 1;
-	else result = ans(now / 2) * 2 + 1;
-
-	return result;
-}
-
 int main() {
-
 	int N, M;
-
 	cin >> N >> M;
-
-	for (int i = 0; i <= N * M; i++)arr[i] = -1;
-	arr[1] = 0;
-	cout << ans(N*M);
-
-
+	cout << N * M - 1;
 	return 0;
 }
