de2c29e7551124d4d30c4bea34cdbb3715594fe8
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-09-02 18:27:54 +0900

    [Gold III] Title: LCA, Time: 28 ms, Memory: 16376 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.cc
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -2,35 +2,51 @@
 #include <vector>
 using namespace std;
 
-vector<int> adj[50002];
-int parent[50002] = { 0, };
-int depth[50002] = { 0, };
+vector<int> adj[100002];
+int parent[100002][18] = {0,};
+int depth[100002] = { 0, };
 int N, M;
+
+int max_dep = 0;
 
 int check(int a, int b) {
 
+	if (a == 1 || b == 1) return 1;
+
 	if (depth[a] != depth[b]) {
 		if (depth[a] > depth[b]) swap(a, b);
-		while (depth[a] != depth[b]) b = parent[b];
+		for (int i = max_dep; i >= 0; i--) {
+			if (depth[parent[b][i]] >= depth[a]) b = parent[b][i];
+		}
+	}
+	int result = b;
+	if (a != b) {
+		for (int i = max_dep; i >= 0; i--) {
+			if (parent[a][i] != parent[b][i]) {
+				a = parent[a][i];
+				b = parent[b][i];
+			}
+			result = parent[b][i];
+		}
 	}
 
-	while (a != b) {
-		a = parent[a];
-		b = parent[b];
-	}
-
-	return a;
+	return result;
 }
 
-void set_tree(int now, int par) {
+void set_tree(int now, int par, int dep) {
 
-	parent[now] = par;
-	depth[now] = depth[par] + 1;
+	depth[now] = dep;
+	parent[now][0] = par;
+
+	for (int i = 1; i <= max_dep; i++) {
+		if (parent[now][i - 1] == 0) break;
+		parent[now][i] = parent[parent[now][i - 1]][i - 1];
+	}
 
 	for (int i = 0; i < adj[now].size(); i++) {
 		int next = adj[now][i];
 		if (next == par) continue;
-		set_tree(next, now);
+		set_tree(next, now, dep + 1);
 	}
 }
 
@@ -40,6 +56,15 @@ int main() {
 
 	cin >> N;
 
+	int temp = N;
+	int cnt = 0;
+
+	while (temp > 1) {
+		temp = temp >> 1;
+		cnt++;
+	}
+	max_dep = cnt;
+
 	for (int i = 0; i < N - 1; i++) {
 		int a, b;
 		cin >> a >> b;
@@ -47,7 +72,7 @@ int main() {
 		adj[b].push_back(a);
 	}
 
-	set_tree(1, 0);
+	set_tree(1, 0, 0);
 
 	cin >> M;
 
