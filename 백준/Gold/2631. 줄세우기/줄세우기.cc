f06766888b05c766175ce19b0021489d435d6201
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-09-21 11:10:32 +0900

    [Gold IV] Title: 줄세우기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.cc
+++ 백준/Gold/2631. 줄세우기/줄세우기.cc
@@ -12,22 +12,17 @@ int main() {
 
 	for (int i = 1; i <= N; i++) {
 		cin >> arr[i];
-		len[i] = 1;
+		len[i] = 0;
 	}
 	int result = 0;
 	for (int i = 1; i <= N; i++) {
-		bool flag = false;
 		int max_length = 0;
 		for (int j = i; j >= 1; j--) {
-			if (len[j] > max_length && arr[j] < arr[i]) {
-				flag = true;
-				max_length = len[j];
-			}
+			if (len[j] > max_length && arr[j] < arr[i]) max_length = len[j];
 		}
-		if(flag) len[i] = max_length + 1;
+		len[i] = max_length + 1;
 		if (result < len[i]) result = len[i];
 	}
-
 	cout << N - result;
 
 	return 0;
