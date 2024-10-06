60ab9d8fc2e7b3c53ecd036c4230c2463b29bd43
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-10-06 20:45:32 +0900

    [Platinum IV] Title: 날카로운 눈, Time: 8 ms, Memory: 2256 KB -BaekjoonHub

--- 백준/Platinum/1637. 날카로운 눈/날카로운 눈.cc
+++ 백준/Platinum/1637. 날카로운 눈/날카로운 눈.cc
@@ -14,21 +14,19 @@ Node arr[20010];
 ll check(ll mid) {
 
 	ll cnt = 0;
-
 	for (int i = 0; i < N; i++) {
-		ll max = (ll)arr[i].c;
+		ll max = arr[i].c;
 		if (max > mid) max = mid;
 
 		if (arr[i].a > max) continue;
 		ll k = (max - arr[i].a) / (ll)arr[i].b + 1;
 		cnt += k;
 	}
-	//cout << cnt << '\n';
 	return cnt;
 }
 
 int main() {
-	//ios_base::sync_with_stdio(false); cin.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
 	cin >> N;
 	for (int i = 0; i < N; i++) cin >> arr[i].a >> arr[i].c >> arr[i].b;
 	
@@ -39,15 +37,12 @@ int main() {
 	while (left <= right) {
 		ll mid = (right + left)/2;
 		
-		//cout << "re : " << left << ", " << right << '\n';
-		
 		if (check(mid) % 2 == 1) {
 			right = mid;
 			result = mid;
 			if (left == right) break;
 		}
 		else left = mid + 1;
-		
 	}
 
 	if (result == -1) cout << "NOTHING";
