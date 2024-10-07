72bd7e0badd9abc449dda29edd461d11104ba3a9
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-10-07 11:43:12 +0900

    [Gold II] Title: 공항, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.cc
+++ 백준/Gold/10775. 공항/공항.cc
@@ -1,9 +1,7 @@
 #include <iostream>
 
 using namespace std;
-
 int G, P, cnt;
-
 int gate[100002];
 
 int Find(int n) {
@@ -13,7 +11,6 @@ int Find(int n) {
 
 bool check(int n) {
 	int next = Find(n);
-	//cout << n << ", " << next << '\n';
 	if (next == 0) return false;
 	else {
 		int ne = Find(next - 1);
@@ -21,23 +18,16 @@ bool check(int n) {
 		gate[next] = ne;
 		return true;
 	}
-
-	
 }
 
 int main() {
-
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> G >> P;
 	for (int i = 1; i <= G; i++) gate[i] = i;
-
 	for (int i = 0; i < P; i++) {
 		int n;
 		cin >> n;
 		if (!check(n))break;
-
-		//for (int i = 1; i <= G; i++) cout << gate[i] << " ";
-		//cout << '\n';
-
 		cnt++;
 	}
 	cout << cnt;
