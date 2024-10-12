da38361262cc5d8dcd8d921a0bd89818dcf55d61
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-10-12 20:04:01 +0900

    [Gold III] Title: 우주신과의 교감, Time: 40 ms, Memory: 9868 KB -BaekjoonHub

---
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#define ll long long
+
+using namespace std;
+
+struct Node {
+	ll row;
+	ll col;
+};
+
+struct Edge {
+	ll cost;
+	int a;
+	int b;
+
+	bool operator<(Edge right) {
+		if (cost < right.cost) return true;
+		return false;
+	}
+};
+
+int N, M;
+
+Node node[1010];
+int par[1010];
+Edge edge[500000];
+
+int Find(int a) {
+	if (par[a] == a) return a;
+	return par[a] = Find(par[a]);
+}
+
+void Union(int a, int b) {
+	int rootA = Find(a);
+	int rootB = Find(b);
+
+	if (rootA == rootB) return;
+
+	par[rootB] = rootA;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) cin >> node[i].row >> node[i].col;
+	for (int i = 1; i <= N; i++) par[i] = i;
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		Union(a, b);
+	}
+	int cnt = 0;
+	for (int i = 1; i <= N; i++) {
+		for (int j = i + 1; j <= N; j++) {
+			ll a = node[i].row - node[j].row;
+			ll b = node[i].col - node[j].col;
+			ll ans = (ll)pow(a, 2) + (ll)pow(b, 2);
+			edge[cnt++] = { ans,i,j };
+		}
+	}
+
+	sort(edge, edge + cnt);
+
+	double result = 0;
+	for (int i = 0; i < cnt; i++) {
+		Edge now = edge[i];
+
+		if (Find(now.a) == Find(now.b)) continue;
+		Union(now.a, now.b);
+		result += sqrt(now.cost);
+		//cout << now.cost << " , " << now.a << ", " << now.b << '\n';
+	}
+	cout << fixed;
+	cout.precision(2);
+	cout << result;
+	
+	return 0;
+}
