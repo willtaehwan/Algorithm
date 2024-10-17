09ceb16765263154b6e0b47bb6a28684c5292086
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-10-17 20:05:55 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 556 ms, Memory: 49120 KB -BaekjoonHub

---
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#define INF 1e9
+
+using namespace std;
+
+struct Node {
+	int row;
+	int col;
+	bool time;
+	int cost;
+	int boom;
+};
+
+int dr[] = { -1,1,0,0 };
+int dc[] = { 0,0,-1,1 };
+
+int N, M, K;
+
+bool map[1002][1002];
+
+int visited[1002][1002][11] = { 0, };
+
+void bfs() {
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			for (int k = 0; k <= K; k++) {
+				visited[i][j][k] = INF;
+			}
+		}
+	}
+
+	queue<Node> q;
+	q.push({ 0,0,0,0,K });
+	visited[0][0][K] = 0;
+
+	while (!q.empty()) {
+		Node now = q.front(); q.pop();
+
+		if (visited[now.row][now.col][now.boom] < now.cost) continue;
+
+		for (int i = 0; i < 4; i++) {
+			int ner = now.row + dr[i];
+			int nec = now.col + dc[i];
+			bool net = !now.time;
+			int neb = now.boom;
+			int nect = now.cost + 1;
+			if (ner < 0 || nec < 0 || ner >= N || nec >= M)continue;
+			if (map[ner][nec]) {
+				if (now.time) {
+					nect++;
+					net = !net;
+				}
+				neb--;
+			}
+			if (neb < 0) continue;
+			if (visited[ner][nec][neb] <= nect) continue;
+
+			visited[ner][nec][neb] = nect;
+
+			q.push({ ner, nec, net, nect, neb });
+		}
+	}
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> M >> K;
+
+	for (int i = 0; i < N; i++) {
+		string str;
+		cin >> str;
+		for (int j = 0; j < M; j++) {
+			map[i][j] = str[j] - '0';
+		}
+	}
+
+	bfs();
+
+	int result = INF;
+	for (int i = 0; i <= K; i++) result = min(result, visited[N - 1][M - 1][i]);
+	if (result == INF) cout << -1;
+	else cout << result + 1;
+
+	return 0;
+}
