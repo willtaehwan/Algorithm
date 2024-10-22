5c3094d4a1514bf9bd54bc47f47895aa4d4ab9fc
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-10-22 09:41:28 +0900

    [Gold III] Title: 선분 교차 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+struct Node {
+	ll x;
+	ll y;
+};
+
+Node p[4];
+
+int ccw(int a, int b, int c) {
+	ll res1 = (p[b].x - p[a].x) * (p[c].y - p[b].y);
+	ll res2 = (p[c].x - p[b].x) * (p[b].y - p[a].y);
+	if (res1 - res2 < 0) return -1;
+	if (res1 - res2 > 0) return 1;
+	return 0;
+}
+
+int main() {
+	bool flag = true;
+	for (int i = 0; i < 4; i++) cin >> p[i].x >> p[i].y;
+
+	if (ccw(0, 1, 2) * ccw(0, 1, 3) >= 0) flag = false;
+	if (ccw(2, 3, 0) * ccw(2, 3, 1) >= 0) flag = false;
+
+	if (flag) cout << 1;
+	else cout << 0;
+
+	return 0;
+}
