f0ad831b150d431f2c841c94be41db57cd372e67
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-10-23 09:57:59 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -8,16 +8,10 @@ struct Node {
 };
 
 Node p[4];
-
-bool flag = true;
 bool in_flag = false;
-
 int ccw(int a, int b, int c) {
 	ll res1 = (p[b].x - p[a].x) * (p[c].y - p[b].y);
 	ll res2 = (p[c].x - p[b].x) * (p[b].y - p[a].y);
-
-	//cout <<a<< ", "<<b<<", "<<c<<" : "<< res1 - res2 << '\n';
-
 	if (res1 - res2 < 0) return -1;
 	if (res1 - res2 > 0) return 1;
 
@@ -27,19 +21,18 @@ int ccw(int a, int b, int c) {
 	ll high_y = max(p[a].y, p[b].y);
 	if (low_x <= p[c].x && high_x >= p[c].x && low_y <= p[c].y && high_y >= p[c].y) in_flag = true;
 
+
 	return 0;
 }
 
 int main() {
-	
+	bool flag = true;
 	for (int i = 0; i < 4; i++) cin >> p[i].x >> p[i].y;
 
-	int case1 = ccw(0, 1, 2) * ccw(0, 1, 3);
-	int case2 = ccw(2, 3, 0) * ccw(2, 3, 1);
-	if (case1 >= 0) flag = false;
-	if (case2 >= 0) flag = false;
+	if (ccw(0, 1, 2) * ccw(0, 1, 3) >= 0) flag = false;
+	if (ccw(2, 3, 0) * ccw(2, 3, 1) >= 0) flag = false;
 
-	if (flag||in_flag) cout << 1;
+	if (flag || in_flag) cout << 1;
 	else cout << 0;
 
 	return 0;
