e216d63f3b05a5f20c3ce4a2654fa8c087f79fd8
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-10-28 22:15:07 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 24 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -2,17 +2,20 @@
 
 using namespace std;
 
-int DP[4002][4002] = { 0, };
+int DP[2][4002] = { 0, };
 
 int main() {
-
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	string str1, str2;
 	cin >> str1 >> str2;
 	int res = 0;
-	for (int i = 1; i <= str1.size(); i++) {
+	for (int i = 0; i < str1.size(); i++) {
+		int n = i % 2;
 		for (int j = 1; j <= str2.size(); j++) {
-			if (str1[i - 1] == str2[j - 1]) DP[i][j] = DP[i - 1][j - 1] + 1;
-			if (DP[i][j] > res) res = DP[i][j];
+			if (str1[i] == str2[j - 1]) DP[n][j] = DP[!n][j - 1] + 1;
+			else DP[n][j] = 0;
+
+			if (DP[n][j] > res) res = DP[n][j];
 		}
 	}
 	cout << res;
