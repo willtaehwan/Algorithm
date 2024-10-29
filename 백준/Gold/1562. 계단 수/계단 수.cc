358fabf17b5a04930c653a811d8e9132fd0709ee
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-10-29 21:54:13 +0900

    [Gold I] Title: 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -3,26 +3,35 @@
 using namespace std;
 
 int N;
-int dp[101][10][1024] = {0, };
+int dp[102][12] = {0, };
+
+int DP(int s, int e) {
+	s++; e++;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < 11; j++) {
+			dp[i][j] = 0;
+		}
+	}
+
+	for (int i = s; i <= e; i++) dp[1][i] = 1;
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = s; j <= e; j++) {
+			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % INF;
+			//if (j > s) dp[i][j] += dp[i - 1][j - 1];
+			//if (j < e) dp[i][j] += dp[i - 1][j + 1];
+			//dp[i][j] %= INF;
+		}
+	}
+	int result = 0;
+	for (int i = max(s,2); i <= e; i++) result = (result + dp[N][i]) % INF;
+	return result;
+}
+
 
 int main() {
 
 	cin >> N;
-
-	for (int i = 1; i < 10; i++) dp[0][i][1 << i] = 1;
-
-	for (int i = 1; i < N; i++) {
-		for (int j = 0; j < 10; j++) {
-			for (int k = 0; k < 1024; k++) {
-				if (j > 0) dp[i][j][k | (1 << j)] += dp[i - 1][j - 1][k];
-				if (j < 9) dp[i][j][k | (1 << j)] += dp[i - 1][j + 1][k];
-				dp[i][j][k | (1 << j)] %= INF;
-			}
-		}
-	}
-
-	int result = 0;
-	for (int i = 0; i < 10; i++) result = (result + dp[N-1][i][1023]) % INF;
-	cout << result;
+	cout << (DP(0, 9) - DP(0, 8) - DP(1, 9) + DP(1, 8) + INF) % INF;
 	return 0;
 }
