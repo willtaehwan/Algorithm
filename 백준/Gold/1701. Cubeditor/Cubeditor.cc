b6abb8a9b898ddaee8aeaee1906f66a906922110
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-11-06 16:29:33 +0900

    [Gold II] Title: Cubeditor, Time: 92 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/1701. Cubeditor/Cubeditor.cc
+++ 백준/Gold/1701. Cubeditor/Cubeditor.cc
@@ -6,12 +6,12 @@ string P;
 int arr[MAX] = { 0, };
 int max_length = 0;
 
-void k_search(int now, int prev) {
+void k_search(int now, int prev, int st) {
 	if (P[prev] == P[now - 1]) {
 		arr[now] = prev + 1;
-		max_length = max(max_length, arr[now]);
+		max_length = max(max_length, arr[now] - st);
 	}
-	else if (prev != 0) k_search(now, arr[prev]);
+	else if (prev != st) k_search(now, arr[prev], st);
 }
 
 int main() {
@@ -19,11 +19,14 @@ int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> P;
-	while(P.size() > 1){
-		for (int i = 0; i <= P.size(); i++) arr[i] = 0;
-		for (int i = 2; i <= P.size(); i++) k_search(i, arr[i - 1]);
-
-		P.erase(P.begin(), P.begin() + 1);
+	
+	for(int st = 0; st<P.size()-1;st++){
+		arr[st] = st;
+		arr[st + 1] = st;
+		for (int i = st + 2; i <= P.size(); i++) {
+			arr[i] = st;
+			k_search(i, arr[i - 1], st);
+		}
 	}
 
 	cout << max_length;
