6d3b534b074d6511bbb5048182fe5febb653072f
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-11-26 03:10:23 +0900

    [Gold I] Title: 최종 순위, Time: 12 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/3665. 최종 순위/최종 순위.cc
+++ 백준/Gold/3665. 최종 순위/최종 순위.cc
@@ -2,31 +2,19 @@
 using namespace std;
 
 int T, N, M;
-bool map[501][501] = { 0, };
 int pre_rank[501] = { 0, };
 int arr[501] = { 0, };
 int now_rank[501] = { 0, };
 
 void init() {
 	cin >> N;
-	for (int i = 0; i <= N; i++) {
-		for (int j = 0; j <= N; j++) {
-			map[i][j] = 0;
-		}
-	}
-	for (int i = 0; i <= N; i++) {
-		now_rank[i] = -1;
-		arr[i] = 0;
-	}
-	for (int i = 0; i < N; i++) cin >> pre_rank[i];
+	for (int i = 0; i <= N; i++) now_rank[i] = -1;
 	
-	for (int i = 0; i < N; i++) {
-		for (int j = i + 1; j < N; j++) {
-			int t = pre_rank[i];
-			int n = pre_rank[j];
-			map[t][n] = 1;
-			arr[t]++;
-		}
+	for (int i = N-1; i >= 0; i--) {
+		int t;
+		cin >> t;
+		pre_rank[t] = i;
+		arr[t] = i;
 	}
 }
 
@@ -41,27 +29,19 @@ int main() {
 		for (int i = 0; i < M; i++) {
 			int a, b;
 			cin >> a >> b;
-			if (map[a][b]) {
-				map[b][a] = 1;
-				map[a][b] = 0;
-				arr[b]++;
+			if (pre_rank[a] > pre_rank[b]) {
 				arr[a]--;
+				arr[b]++;
 			}
 			else {
-				map[a][b] = 1;
-				map[b][a] = 0;
 				arr[a]++;
 				arr[b]--;
 			}
 		}
 
 		for (int i = 1; i <= N; i++) {
-			int r = arr[i];
-			if (now_rank[r] != -1) {
-				flag = false;
-				break;
-			}
-			now_rank[r] = i;
+			if (now_rank[arr[i]] != -1) flag = false;
+			now_rank[arr[i]] = i;
 		}
 
 		if (!flag) cout << "IMPOSSIBLE\n";
