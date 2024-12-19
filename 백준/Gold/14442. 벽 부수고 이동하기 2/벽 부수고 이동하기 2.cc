93ba42ae7b6efe9f1c4068e51d7d9a99951eafa5
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-12-19 21:38:13 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 244 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -6,32 +6,33 @@ struct Node {
 	int row;
 	int col;
 	int cnt;
+	int dis;
 };
 
 int N, M, K;
 bool map[1001][1001] = { 0, };
-int n_map[1000][1001][11] = { 0, };
+int n_map[1000][1001] = { 0, };
 
 int dr[] = { -1,1,0,0 };
 int dc[] = { 0,0,-1,1 };
 
-void bfs() {
+int bfs() {
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			for (int k = 0; k <= K; k++) {
-				n_map[i][j][k] = 1e9;
-			}
+			n_map[i][j] = -1;
 		}
 	}
 
 	queue<Node> q;
-	q.push({ 0, 0, K });
-	n_map[0][0][K] = 1;
+	q.push({ 0, 0, K,1 });
+	n_map[0][0] = 1;
 
 
 	while (!q.empty()) {
 		Node now = q.front(); q.pop();
+
+		if (now.row == N - 1 && now.col == M - 1) return now.dis;
 
 		for (int i = 0; i < 4; i++) {
 			int ner = now.row + dr[i];
@@ -40,14 +41,15 @@ void bfs() {
 			if (ner < 0 || nec < 0 || ner >= N || nec >= M) continue;
 			if (map[ner][nec]) nect--;
 			if (nect < 0) continue;
-			if (n_map[ner][nec][nect] <= n_map[now.row][now.col][now.cnt] + 1) continue;
-
-			n_map[ner][nec][nect] = n_map[now.row][now.col][now.cnt] + 1;
-			q.push({ ner,nec,nect });
+			if (n_map[ner][nec] < nect || n_map[ner][nec] == -1) {
+				n_map[ner][nec] = nect;
+				q.push({ ner,nec,nect, now.dis + 1 });
+			}
 		}
 
 	}
 
+	return -1;
 }
 
 int main() {
@@ -62,24 +64,7 @@ int main() {
 		}
 	}
 
-	
-	bfs();
-	/*for (int k = 0; k <= K; k++) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (n_map[i][j][k] == 1e9) cout << "X ";
-				else cout << n_map[i][j][k] << " ";
-			}
-			cout << '\n';
-		}
-		cout << "\n\n";
-	}*/
-	
-	int result = 1e9;
-	for (int i = 0; i <= K; i++) result = min(result, n_map[N - 1][M - 1][i]);
-	
-	if (result == 1e9) cout << -1;
-	else cout << result;
+	cout << bfs();
 	
 	return 0;
 }
