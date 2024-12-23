4769b3c4bbb13eb33a79114f0bf1743d83f81558
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-12-23 21:04:53 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -18,12 +18,9 @@ int main() {
 			if (cnt % 3 != i) result += DP[i];
 		}
 		DP[cnt%3] = result % 15746;
-		
 		cnt++;
 	}
-	if (N == 1) cout << 1;
-	else if (N == 2) cout << 2;
-	else cout << DP[(cnt-1)%3];
+	cout << DP[(N-1)%3];
 
 	return 0;
 }
