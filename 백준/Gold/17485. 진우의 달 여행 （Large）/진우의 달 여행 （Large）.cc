722d96c06cc1f01fc3dfe50404100455e26e8abe
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2024-12-29 23:15:47 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 80 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+
+int map[1002][1002] = { 0, };
+int DP[1002][1002][3] = {0,};
+
+int main() {
+
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> map[i][j];
+			for (int k = 0; k < 3; k++) {
+				if (i == 0) DP[i][j][k] = map[i][j];
+				else DP[i][j][k] = 1e9;
+			}
+		}
+	}
+	int min_result = 1e9;
+	for (int i = 1; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			for (int k = 0; k < 3; k++) {
+				int prc = j + k - 1;
+				if (prc < 0 || prc >= M) continue;
+				for (int l = 0; l < 3; l++) {
+					if (k == l) continue;
+					DP[i][j][k] = min(DP[i][j][k], DP[i - 1][j + k - 1][l] + map[i][j]);
+					if (i == N - 1) min_result = min(min_result, DP[i][j][k]);
+				}
+			}
+		}
+	}
+
+	cout << min_result;
+
+	return 0;
+
+}
