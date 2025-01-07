60af928cf06b657af2a8a47cdfe0c13780a3d0d8
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-01-07 21:41:31 +0900

    [Gold IV] Title: 전구와 스위치, Time: 0 ms, Memory: 2684 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.cc
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.cc
@@ -9,9 +9,6 @@ bool tar[100001] = { 0, };
 int min_result = 1e9;
 
 void switch_on(int ch, int idx, int cnt) {
-	
-	//for (int i = 0; i < N; i++)cout << arr[ch][i];
-	//cout <<"  :  " << cnt << '\n';
 
 	if (idx == N) {
 		if (tar[N -1] == arr[ch][N - 1]) min_result = min(min_result, cnt);
@@ -41,16 +38,8 @@ int main() {
 	arr[1][0] = !(arr[1][0]);
 	arr[1][1] = !(arr[1][1]);
 
-	/*for (int i = 0; i < N; i++) cout << arr[0][i];
-	cout << '\n';
-	for (int i = 0; i < N; i++) cout << arr[1][i];
-	cout << '\n';
-	for (int i = 0; i < N; i++) cout << tar[i];
-	cout << "\n\n";*/
 
 	switch_on(0, 1, 0);
-
-	//cout << '\n';
 
 	switch_on(1, 1, 1);
 
