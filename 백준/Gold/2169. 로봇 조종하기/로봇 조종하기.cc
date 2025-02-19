4686809f44cfc3826b5112101ac711800a6ac06f
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-02-19 21:04:35 +0900

    [Gold II] Title: 로봇 조종하기, Time: 72 ms, Memory: 6020 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.cc
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 int N, M;
 int map[1010][1010] = { 0, };
-int DP[1010][1010][2];
+int DP[2][1010][2];
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -14,28 +14,29 @@ int main() {
 		}
 	}
 	int tot = 0;
-	for (int i = 1; i <= M; i++) {
-		tot += map[1][i];
-		DP[1][i][0] = tot;
-		DP[1][i][1] = tot;
+	for (int j = 1; j <= M; j++) {
+		tot += map[1][j];
+		DP[1][j][0] = tot;
+		DP[1][j][1] = tot;
 	}
-
+	int result = 0;
 	for (int i = 2; i <= N; i++) {
+		int n = i % 2;
 		for (int j = 0; j <= M + 1; j++) {
-			DP[i][j][0] = -1e9;
-			DP[i][j][1] = -1e9;
+			DP[n][j][0] = -1e9;
+			DP[n][j][1] = -1e9;
 		}
 
 		for (int j = 1; j <= M; j++) {
 			int rev = M - j + 1;
-			int prev_l = max(DP[i - 1][j][0], DP[i - 1][j][1]);
-			int prev_r = max(DP[i - 1][rev][0], DP[i - 1][rev][1]);
-			DP[i][j][0] = max(prev_l, DP[i][j - 1][0]) + map[i][j];
-			DP[i][rev][1] = max(prev_r, DP[i][rev + 1][1]) + map[i][rev];
+			int prev_l = max(DP[!n][j][0], DP[!n][j][1]);
+			int prev_r = max(DP[!n][rev][0], DP[!n][rev][1]);
+			DP[n][j][0] = max(prev_l, DP[n][j - 1][0]) + map[i][j];
+			DP[n][rev][1] = max(prev_r, DP[n][rev + 1][1]) + map[i][rev];
 		}
 	}
 
-	cout << max(DP[N][M][0], DP[N][M][1]);
+	cout << max(DP[N%2][M][0], DP[N%2][M][1]);
 
 	return 0;
 }
