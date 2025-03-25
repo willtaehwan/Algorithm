8823df781c0b2e730ad51d3cc9fe07ac19ad8b49
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-03-25 19:22:47 +0900

    [Gold III] Title: 나머지 합, Time: 112 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,26 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int N, M;
-int arr[1002] = { 0, };
+int N, M, a, s = 0;
+unsigned int r[1002] = { 1 };
+long long ans = 0;
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
 	cin >> N >> M;
-
-	int sum = 0;
-	arr[0] = 1;
-	for (int i = 0; i < N; i++) {
-		int a;
+	while (N--) {
 		cin >> a;
-		sum = (sum + a) % M;
-		arr[sum]++;
-	}
-	long long ans = 0;
-	for (int i = 0; i < M; i++) {
-		long long n = arr[i];
-		if (n == 0 || n == 1) continue;
-		ans += (n * (n - 1)) / 2;
+		s = (s + a) % M;
+		ans += r[s]++;
 	}
 	cout << ans;
 	return 0;
