cc3adfde57f4ed3fcad196a5ec892b072e20218d
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-04-07 20:35:35 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -9,7 +9,7 @@ struct Edge {
 };
 
 int N, M, W;
-Edge v[6001];
+Edge v[5201];
 int dist[502] = { 0, };
 
 int main() {
@@ -28,30 +28,15 @@ int main() {
 			else t *= -1;
 			v[cnt++] = { s,e,t };
 		}
-		
-		for (int i = 2; i <= N; i++) dist[i] = 1e9;
-		dist[1] = 0;
-		/*cout << '\n';
-		for (int i = 0; i < cnt; i++) cout << v[i].s << " " << v[i].e << " " << v[i].t << '\n';
-		cout << '\n';*/
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < cnt; j++) {
-				Edge now = v[j];
-				//if (dist[now.s] == 1e9) continue;
-				if (dist[now.e] > dist[now.s] + now.t) dist[now.e] = dist[now.s] + now.t;
-			}
-		}
-
-		//for (int i = 1; i <= N; i++) cout << dist[i] << " ";
-		//cout << '\n';
 
 		bool flag = false;
-		for (int i = 0; i < cnt; i++) {
-			Edge now = v[i];
-			//if (dist[now.s] == 1e9) continue;
-			if (dist[now.e] > dist[now.s] + now.t) {
-				flag = true;
-				break;
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j < cnt; j++) {
+				Edge now = v[j];
+				if (dist[now.e] > dist[now.s] + now.t) {
+					dist[now.e] = dist[now.s] + now.t;
+					if (i == N) flag = true;
+				}
 			}
 		}
 
