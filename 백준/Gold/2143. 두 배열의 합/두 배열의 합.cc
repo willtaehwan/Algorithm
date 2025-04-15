ca61029eccea930b5f1c5f08584d87fe9f65b634
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-04-15 16:11:43 +0900

    [Gold III] Title: 두 배열의 합, Time: 60 ms, Memory: 8304 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <unordered_map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int T, N, M;
@@ -7,8 +8,8 @@ int T, N, M;
 int A[1002] = { 0, };
 int B[1002] = { 0, };
 
-unordered_map<int, long long> mA;
-unordered_map<int, long long> mB;
+vector<int> sA;
+vector<int> sB;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -20,32 +21,54 @@ int main() {
 		cin >> a;
 		A[i] = A[i - 1] + a;
 	}
-
 	cin >> M;
 	for (int i = 1; i <= M; i++) {
 		int b;
 		cin >> b;
 		B[i] = B[i - 1] + b;
 	}
-
-	
 	for (int i = 1; i <= N; i++) {
 		for (int j = 0; j < i; j++) {
-			mA[A[i] - A[j]]++;
+			sA.push_back(A[i] - A[j]);
 		}
 	}
 
 	for (int i = 1; i <= M; i++) {
 		for (int j = 0; j < i; j++) {
-			mB[B[i] - B[j]]++;
+			sB.push_back(B[i] - B[j]);
 		}
 	}
-	long long ans = 0;
-	for (auto it : mA) {
-		if(mB.find(T-it.first) != mB.end()) ans += it.second * mB[T - it.first];
-	}
-	cout << ans;
 
+	sort(sA.begin(), sA.end());
+	sort(sB.begin(), sB.end());
+	
+	long long ans = 0;
+	int na = sA.size() - 1;
+	int nb = 0;
+	long long cnt = 1;
+	while (1) {
+
+		if (na < 0 || nb >= sB.size()) break;
+
+		if (sA[na] + sB[nb] > T) {
+			na--;
+			cnt = 1;
+		}
+		else if (sA[na] + sB[nb] < T) {
+			nb++;
+			cnt = 1;
+		}
+		else if (na > 0 && sA[na] == sA[na - 1]) {
+			cnt++;
+			na--;
+		}
+		else {
+			ans += cnt;
+			nb++;
+		}
+	}
+
+	cout << ans;
 
 	return 0;
 }
