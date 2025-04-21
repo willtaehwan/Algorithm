cc839da9821b657e0e69bf2b23ae39f80eb5b8e0
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-04-21 23:08:22 +0900

    [Gold II] Title: 빵집, Time: 88 ms, Memory: 6912 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.cc
+++ 백준/Gold/3109. 빵집/빵집.cc
@@ -5,28 +5,17 @@ using namespace std;
 int N, M;
 int id = 1;
 int ans = 0;
-int map[10001][501] = { 0, };
+bool map[10001][501] = { 0, };
 
-void go(int r, int c, int idx) {
+bool go(int r, int c) {
 
-	if (c == M - 1) {
-		ans++;
-		id++;
-		return;
-	}
+	if (c == M - 1) return 1;
+	if (r < 0 || c < 0 || r >= N || c >= M) return 0;
+	if (map[r][c]) return 0;
 
-	for (int i = -1; i < 2; i++) {
-		if (id != idx) return;
-		int ner = r + i;
-		int nec = c + 1;
-		if (ner < 0 || nec < 0 || ner >= N || nec >= M) continue;
-		if (map[ner][nec] != 0) continue;
+	map[r][c] = 1;
 
-		map[ner][nec] = idx;
-		go(ner, nec, idx);
-		
-	}
-
+	return (go(r-1, c+1) || go(r, c+1) || go(r+1, c+1));
 }
 
 int main() {
@@ -37,20 +26,12 @@ int main() {
 		for (int j = 0; j < M; j++) {
 			char c;
 			cin >> c;
-			if (c == 'x') map[i][j] = -1;
+			if (c == 'x') map[i][j] = 1;
 			else map[i][j] = 0;
 		}
 	}
 
-	for (int i = 0; i < N; i++) go(i, 0, id);
-
-	/*for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cout << map[i][j]<<' ';
-		}
-		cout << '\n';
-	}*/
-
+	for (int i = 0; i < N; i++) ans += go(i, 0);
 	cout << ans;
 
 	return 0;
