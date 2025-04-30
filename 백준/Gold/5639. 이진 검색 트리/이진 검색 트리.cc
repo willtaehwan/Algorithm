9c12a0d2663a2dc6c046bafb4253b4abdfd36d5f
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-04-30 21:19:05 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 28 ms, Memory: 2500 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -2,63 +2,39 @@
 #include <vector>
 using namespace std;
 
-struct Node {
-	int p;
-	int l;
-	int r;
-};
+vector<int> n;
 
-Node n[1000001];
+void post(int st, int ed) {
 
-void o_o(int a) {
+	if (st >= ed) return;
 
-	if (n[a].l != 0) o_o(n[a].l);
-	if (n[a].r != 0) o_o(n[a].r);
-	if (a != 0) cout << a <<"\n";
+	int i = st + 1;
+	for (; i < ed; i++) {
+		if (n[st] < n[i]) break;
+	}
+
+	post(st + 1, i);
+	post(i, ed);
+	cout << n[st] << '\n';
 }
 
-void p_o(int p, int a) {
-
-	if (p > a) {
-		if (n[p].l == 0) {
-			n[p].l = a;
-			n[a].p = p;
-			return;
-		}
-		p_o(n[p].l, a);
-	}
-	else {
-		if (n[p].r == 0) {
-			n[p].r = a;
-			n[a].p = p;
-			return;
-		}
-		p_o(n[p].r, a);
-	}
-}
 
 int main() {
 
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int a;
-    
-	/*
-	vector<int> v;
+	/*int N;
+	cin >> N;
+
+	int idx = 0;
 	for (int i = 0; i < N; i++) {
 		cin >> a;
-		v.push_back(a);
-		p_o(0, a);
-	}
+		n.push_back(a);
+	}*/
 
-	for (int i = 0; i < v.size(); i++) {
-		int now = v[i];
-		cout << now<<" "<<n[now].p << " " << n[now].l << " " << n[now].r << '\n';
-	}
-	*/
+	while (cin >> a) n.push_back(a);
 
-	while (cin >> a) p_o(0, a);
-
-	o_o(0);
+	post(0, n.size());
 
 	return 0;
 }
