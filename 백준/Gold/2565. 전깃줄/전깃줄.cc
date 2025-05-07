6332dd400dd8495db39b2e18af5b092c9aaae71f
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-05-07 17:42:10 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -1,53 +1,37 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 int N;
-int arr[502] = { 0, };
-vector<int> v;
-int idx = 0;
+int arr[102] = { 0, };
 
-void search_bi(int a) {
-
-	int l = 0;
-	int r = idx;
-	int mid = (l + r) / 2;
-
-	while (l < r) {
-		mid = (l + r) / 2;
-		if (v[mid] > a) r = mid;
-		else if (v[mid] < a) l = mid + 1;
-		else break;
-	}
-
-	v[l] = a;
-}
+vector<pair<int, int>> v;
 
 int main() {
 
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
 
-	int M = 0;
 	for (int i = 0; i < N; i++) {
 		int a, b;
 		cin >> a >> b;
-		arr[a] = b;
-		M = max(M, a);
+		v.push_back({ a,b });
 	}
 
-	v.push_back(0);
-	for (int i = 1; i <= M; i++) {
-		int a = arr[i];
-		if (a == 0) continue;
-		if (v[idx] > a) search_bi(a);
-		else if (v[idx] < a) {
-			v.push_back(a);
-			idx++;
+	sort(v.begin(), v.end());
+
+	int m = 0;
+	for (int i = 0; i < N; i++) {
+		arr[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (v[i].second <= v[j].second) continue;
+			arr[i] = max(arr[i], arr[j] + 1);
 		}
+		m = max(m, arr[i]);
 	}
 
-	cout << N - idx;
+	cout << N - m;
 
 	return 0;
 }
