ff7303392989e4b0129a2b5d2de8b6b710fc3d4c
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-07-24 09:39:29 +0900

    [Gold IV] Title: Security Guards, Time: 484 ms, Memory: 106144 KB -BaekjoonHub

--- 백준/Gold/16346. Security Guards/Security Guards.cc
+++ 백준/Gold/16346. Security Guards/Security Guards.cc
@@ -1,22 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int N, Q;
+struct Node {
+	int row;
+	int col;
+};
 
+int N, Q;
+int mr = 0;
+int mc = 0;
 int arr[5005][5005] = { 0, };
+
+vector<Node> v;
 
 int bi_search(int row, int col) {
 
 	int l = 0;
-	int r = 5000;
+	int r = max(mr, mc);
 
 	while (l < r) {
 		int mid = (l + r) / 2;
 
 		int br = (row - mid) < 1 ? 1 : (row - mid);
 		int bc = (col - mid) < 1 ? 1 : (col - mid);
-		int tr = (row + mid) > 5000 ? 5001 : (row + mid);
-		int tc = (col + mid) > 5000 ? 5001 : (col + mid);
+		int tr = (row + mid) > mr ? mr : (row + mid);
+		int tc = (col + mid) > mc ? mc : (col + mid);
 
 		int now = arr[tr][tc] - arr[br - 1][tc] - arr[tr][bc - 1] + arr[br - 1][bc - 1];
 
@@ -32,8 +41,7 @@ int main() {
 
 	cin >> N >> Q;
 
-	int mr = 0;
-	int mc = 0;
+
 	for (int i = 0; i < N; i++) {
 		int r, c;
 		cin >> r >> c;
@@ -42,16 +50,22 @@ int main() {
 		mc = max(mc, c+1);
 	}
 
-	for (int i = 1; i <= 5001; i++) {
-		for (int j = 1; j <= 5001; j++) {
+	for (int i = 0; i < Q; i++) {
+		int r, c;
+		cin >> r >> c;
+		v.push_back({ r + 1, c + 1 });
+		mr = max(mr, r + 1);
+		mc = max(mc, c + 1);
+	}
+
+	for (int i = 1; i <= mr; i++) {
+		for (int j = 1; j <= mc; j++) {
 			arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
 		}
 	}
 
-	for (int i = 0; i < Q; i++) {
-		int r, c;
-		cin >> r >> c;
-		cout << bi_search(r + 1, c + 1)<<'\n';
+	for (int i = 0; i < v.size(); i++) {
+		cout << bi_search(v[i].row, v[i].col) << '\n';
 	}
 
 	return 0;
