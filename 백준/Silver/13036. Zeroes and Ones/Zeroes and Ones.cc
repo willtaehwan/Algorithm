76f6c5d96c6001442fe9dfb0c54b2d77de741f76
willtaehwan <128010871+willtaehwan@users.noreply.github.com>
2025-08-04 09:30:57 +0900

    [Silver I] Title: Zeroes and Ones, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Silver/13036. Zeroes and Ones/Zeroes and Ones.cc
+++ 백준/Silver/13036. Zeroes and Ones/Zeroes and Ones.cc
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-/*
-
-0110 -> 0000 -> 1100 -> 1111
-
-0110 -> 1010 -> 1100 -> 1111
-
-1111
-
-
-*/
 int T;
 
 int main() {
@@ -24,20 +14,21 @@ int main() {
 
 		string a, b;
 		cin >> a >> b;
-		a.push_back('0');
 		int cnt = 0;
 		bool flag = true;
 		for (int i = 0; i < N; i++) {
 			if (a[i] == b[i]) continue;
+			if (i == N - 1) {
+				flag = false;
+				continue;
+			}
 			if (a[i + 1] == '0') a[i + 1] = '1';
 			else a[i + 1] = '0';
 			cnt++;
-			if (i == N - 1) flag = false;
 		}
 		if (flag) cout << cnt<<'\n';
 		else cout << -1<<'\n';
 	}
-
 
 	return 0;
 }
